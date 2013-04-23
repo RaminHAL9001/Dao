142f745d4a7b9d2557a0240148d34695f12899d9
Ramin Honary <ramin.honary@gmail.com>
2013-04-24 03:50:59 +0900

    Created a Prelude for the Dao system, which is really intended to be used from GHCi, but should also be useful from other programs that wish to import the Dao system.

--- src/Dao/Prelude.hs
+++ src/Dao/Prelude.hs
@@ -1,6 +1,5 @@
--- "src/Dao/Prelude.hs"  provides Haskell language functions necessary
--- to construct a knowledge base of pattern->rule relationships that can
--- be executed in the Dao system's runtime.
+-- "src/Dao/Prelude.hs"  a module to provide convenient APIs to the
+-- internals of the Dao system, especially designed for use in GHCi.
 -- 
 -- Copyright (C) 2008-2013  Ramin Honary.
 -- This file is part of the Dao System.
@@ -19,91 +18,173 @@
 -- along with this program (see the file called "LICENSE"). If not, see
 -- <http://www.gnu.org/licenses/agpl.html>.
 
-module Dao.Prelude
-  ( rule, pattern, action
-  )
-  where
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Dao.Prelude where
+
+import qualified Data.ByteString.Lazy   as B
+ 
+-- | This type  for all commands in this module to "pipe" everything together like in the Bourne
+-- scripting language where you can pipe the output of one command to the input of another, or like
+-- Haskell "arrows", except with the monadic bind 'Control.Monad.(>>=)' operator (monads are a
+-- subcategory of arrows anyway).
+
+import           RandObj
 
 import           Dao.String
+import qualified Dao.Tree as T
 import           Dao.Object
-import           Dao.Glob
+import           Dao.PPrint
+import           Dao.Struct
+import           Dao.Token
+import           Dao.Parser
 
-import           Data.Binary
-import qualified Data.ByteString.Lazy      as B
+import           Dao.Object.PPrint
+import           Dao.Object.AST
+import           Dao.Object.Parser
+import           Dao.Object.Struct
 
-data Event = SETUP | BEGIN | END | TAKEDOWN deriving (Eq, Ord, Read, Show, Enum)
+import           Control.Exception
+import           Control.Monad
+import           Control.Monad.State
 
-data Builder a
-  = Monadic { ruleStateMonad :: State RuleGroup a }
-  | AddRule [Glob] DaoFunc a
-  | AddEvent Event DaoFunc a
-  | AddFunc DaoFunc a
+----------------------------------------------------------------------------------------------------
 
-toMonadic :: RuleSet a -> RuleSet a
-toMonadic a = Monadic $ case a of
-  Monadic a -> a
-  AddRule pat act a -> do
-    modify (\st -> st{ ruleSet = T.unionWith (++) p a (ruleSet st) })
-    return a
-  AddEvent evt action a -> do
-    st <- get
-    case evt of
-      SETUP    -> put (st{ setupEvents    = setupEvents    st ++ [action] })
-      BEGIN    -> put (st{ beginEvents    = beginEvents    st ++ [action] })
-      END      -> put (st{ endEvents      = endEvents      st ++ [action] })
-      TAKEDOWN -> put (st{ takedownEvents = takedownEvents st ++ [action] })
-    return a
-  AddFunc func a -> do
-    modify $ \st ->
-      let i = 1 + funcCounter st
-      in st{ funcCounter = i, funcMap = I.insert i func (funcMap st) }
-    return a
+-- | Everything is wrapped in this type to prevent GHCi from printing stuff all the time, or
+-- reporting errors about types not instantiating the 'Prelude.Show' class.
+newtype O a = O { uno :: a }
+instance Show (O a) where { show = "OK" }
 
-instance Monad Builder where
-  return a = Monadic (return a)
-  fa >>= mfa =
-    Monadic{ ruleStateMonad = ruleStateMonad (toMonadic fa) >>= ruleStateMonad . toMonadic . mfa }
+randMaxDepth :: Int
+randMaxDepth = 6
 
--- | This makes the syntax for adding events simple. For example:
--- @SETUP ... do@
--- @    ref "a.b.c" := obj "hello"@
-(...) :: Event -> Action -> Builder ()
-(...) evt act = AddEvent evt act ()
+-- | Generate a random object of a polymorphic type with a specified maximum depth.
+ioRandOMax :: HasRandGen o => Int -> Int -> IO (O o)
+ioRandOMax max i = return (O (evalState randO (initRandState max i)))
 
-class ToPattern pat where
-  toPattern :: pat -> [Glob]
+-- | Generate a random object of a polymorphic type.
+ioRandO :: HasRandGen o => Int -> IO (O o)
+ioRandO i = ioRandOMax randMaxDepth i
 
-instance ToPattern Glob where { toPattern pat = [pat] }
-instance ToPattern [Glob] where { toPattern = id }
-instance ToPattern String where { toPattern str = [read str] }
-instance ToPattern [String] where { toPattern = map read }
+-- | Generate a random 'Dao.Object.AST.AST_TopLevel' expression from a seed value.
+randTopExpr :: Int -> IO (O AST_TopLevel)
+randTopExpr = ioRandO
 
-rule :: ToPattern pat => pat -> Action ig -> Builder ()
-rule pat act = if null pat then return () else AddRule (toPattern pat) (act >> return ())
+-- | Generate a random 'Dao.Object.AST.AST_Script' from a seed value.
+randScriptExpr :: Int -> IO (O AST_Script)
+randScriptExpr = ioRandO
 
--- A 'DaoFunc' is defined so that you can basically gather your variables from input arguments, and
--- then define an action for it like so:
--- @function $
--- @  [do  ORef a <- arg "a" -- gets the first argument, calls it "a" in error messages.@
--- @       OInt b <- arg "b" -- gets the second argument, calls it "b" in error messages.@
--- @       begin $ do@
--- @           action :: Action a@
--- @  ]@
-data GetArgs a
-  = GetArgs
-    { daoFuncToIO :: IO (PValue a) -- ^ is in the IO monad to catch pattern matching exceptions.
-    }
-  | TryAction
-    { getArgsAction :: Action a
-    } -- ^ If 'TryAction' can be evaluated without an exception, further monadic evaluation will not
-      -- continue beyond it,
-  | GotException SomeException
-      -- ^ Semantically equivalent to 'Control.Monad.mzero'.
+-- | Generate a random 'Dao.Object.AST.AST_Object' from a seed value.
+randObjExpr :: Int -> IO (O AST_Object)
+randObjExpr = ioRandO
 
-function :: [GetArgs ig] -> Builder ()
-function funcs = AddFunc (msum funcs >> return ()) ()
+-- | Generate a random 'Dao.Object.Object' value from a seed value.
+randObj :: Int -> IO (O Object)
+randObj = ioRandO
 
--- | Begin the 'Action' part of a 'DaoFunc' after retrieving the arguments. Specify multiple
-begin :: Action a -> GetArgs a
-begin = TryAction
+-- | Generate a random 'Dao.Object.AST.AST_TopLevel' expression from a seed value with a specified
+-- maximum depth.
+randTopExprMax :: Int -> Int -> IO (O AST_TopLevel)
+randTopExprMax = ioRandOMax
+
+-- | Generate a random 'Dao.Object.AST.AST_Script' from a seed value with a specified maximum depth.
+randScriptExprMax :: Int -> Int -> IO (O AST_Script)
+randScriptExprMax = ioRandOMax
+
+-- | Generate a random 'Dao.Object.AST.AST_Object' from a seed value with a specified maximum depth.
+randObjExprMax :: Int -> Int -> IO (O AST_Object)
+randObjExprMax = ioRandOMax
+
+-- | Generate a random 'Dao.Object.Object' value from a seed value with a specified maximum depth.
+randObjMax :: Int -> Int -> IO (O Object)
+randObjMax = ioRandOMax
+
+-- | Pretty-print anything output by one of the functions in this module.
+pp :: PPrintable a => (O a) -> IO (O a)
+pp (O o) = putStrLn (prettyShow o) >> return (O o)
+
+-- | Convert an object to a string using the pretty printer.
+toString :: PPrintable a => (O a) -> IO (O String)
+toString (O o) = return (O (prettyShow o))
+
+-- | Parser a polymorphic type from a string expression.
+parseWith :: Parser a -> O String -> IO (O a)
+parseWith parser (O str) = case runParser parser str of
+  (OK          a, _) -> seq a $! return (O a)
+  (Backtrack    , s) -> ioError ("parser backtracks:\n\t"++show s)
+  (PFail loc msg, s) -> ioError (show loc ++ ": "++uchars msg++"\n\t"++show s)
+
+-- | Parse a 'Dao.Object.AST.AST_TopLevel'.
+parseTopExpr :: O String -> IO (O AST_TopLevel)
+parseTopExpr = parseWith parseDirective
+
+-- | Parse a 'Dao.Object.AST.AST_Script'.
+parseScriptExpr :: O String -> IO (O AST_TopLevel)
+parseScriptExpr = parseWith parseScriptExpr
+
+-- | Parse a 'Dao.Object.AST.AST_Object'.
+parseScriptExpr :: O String -> IO (O AST_Object)
+parseScriptExpr = parseWith parseObjectExpr
+
+-- | Parse a 'Dao.Object.AST.AST_TopLevel' from an ordinary 'Prelude.String'.
+readTopExpr :: String -> IO (O AST_TopLevel)
+readTopExpr = parseTopExpr . O
+
+-- | Parse a 'Dao.Object.AST.AST_TopLeve' from an ordinary 'Prelude.String'.
+readScriptExpr :: String -> IO (O AST_TopLevel)
+readScriptExpr = parseScriptExpr . O
+
+-- | Parse a 'Dao.Object.AST.AST_TopLeve' from an ordinary 'Prelude.String'.
+readObjExpr :: String -> IO (O AST_TopLevel)
+readObjExpr = parseObjExpr . O
+
+-- | Convert a structured item to its structured form.
+toStruct :: Structured o => (O o) -> IO (O (T.Tree Name Object))
+toStruct (O o) = return (O (dataToStruct o))
+
+-- | Construct a random object of a polymorphic type from its 'Dao.Struct.Structured' form.
+fromStruct :: Structured o => (O (T.Tree Name Object)) -> IO (O o)
+fromStruct (O t) = return (O (structToData t))
+
+-- | Construct a random 'Dao.Object.AST.AST_TopLevel' expression from its 'Dao.Struct.Structured' form.
+structTopExpr :: O (T.Tree Name Object) -> IO (O AST_TopLevel)
+structTopExpr = fromStruct
+
+-- | Construct a random 'Dao.Object.AST.AST_Script' expression from its 'Dao.Struct.Structured' form.
+structScriptExpr :: O (T.Tree Name Object) -> IO (O AST_Script)
+structScriptExpr = fromStruct 
+
+-- | Construct a random 'Dao.Object.AST.AST_Object' expression from its 'Dao.Struct.Structured' form.
+structObjExpr :: O (T.Tree Name Object) -> IO (O AST_Object)
+structObjExpr = fromStruct 
+
+-- | Construct a random 'Dao.Object.Object.AST_Object' expression from its 'Dao.Struct.Structured' form.
+structObjExpr :: O (T.Tree Name Object) -> IO (O AST_Object)
+structObjExpr = fromStruct 
+
+-- | Convert a polymorphic type to a 'Data.ByteString.Lazy.ByteString'.
+toBinary :: Binary o => O o -> IO (O B.ByteString)
+toBinary o = return (encode o)
+
+-- | Convert a polymorphic type to a 'Data.ByteString.Lazy.ByteString'.
+fromBinary :: Binary o => O B.ByteString -> IO (O o)
+fromBinary (O o) = return (encode o)
+
+-- | Parse a 'Dao.Object.TopLevelExpr' from it's binary representation.
+unbinTopExpr :: O B.ByteString -> IO TopLevelExpr
+unbinTopExpr = fromBinary
+
+-- | Parse a 'Dao.Object.ScriptExpr' from it's binary representation.
+unbinScriptExpr :: O B.ByteString -> IO ScriptExpr
+unbinScriptExpr = fromBinary
+
+-- | Parse a 'Dao.Object.ScriptExpr' from it's binary representation.
+unbinObjExpr :: O B.ByteString -> IO ObjectExpr
+unbinObjExpr = fromBinary
+
+-- | Parse a 'Dao.Object.ScriptExpr' from it's binary representation.
+unbinObj :: O B.ByteString -> IO Object
+unbinObj = fromBinary
+
+
 

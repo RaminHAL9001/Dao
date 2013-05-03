895e107d020f9d5c138c47d3a48aabadaa6ed9b2
Ramin Honary <ramin.honary@gmail.com>
2013-05-03 17:44:03 +0900

    The object wrapper 'Dao.Prelude.O' has been renamed to 'Dao.Prelude.Dao', now all Dao types used in the "Dao.Prelude" module are wrapped in a simple newtype called 'Dao'.

--- src/Dao/Prelude.hs
+++ src/Dao/Prelude.hs
@@ -79,60 +79,60 @@ import           System.IO
 
 -- | Everything is wrapped in this type to prevent GHCi from printing stuff all the time, or
 -- reporting errors about types not instantiating the 'Prelude.Show' class.
-newtype O a = O { uno :: a }
-instance Typeable a => Show     (O a) where { show (O a)          = show (typeOf a) }
-instance Eq a     => Eq         (O a) where { (O a)==(O b)        = a==b            }
-instance Ord a    => Ord        (O a) where { compare (O a) (O b) = compare a b     }
-instance             Functor     O    where { fmap fn (O a)       = O (fn a)        }
-instance Monoid a => Monoid     (O a) where
-  mempty              = O mempty
-  mappend (O a) (O b) = O (mappend a b)
-instance             Applicative O    where
-  pure                = O
-  (O fn) <*> (O a)    = O (fn a)
+newtype Dao a = Dao { daoUnwrap :: a }
+instance Typeable a => Show     (Dao a) where { show (Dao a)            = show (typeOf a) }
+instance Eq a     => Eq         (Dao a) where { (Dao a)==(Dao b)        = a==b            }
+instance Ord a    => Ord        (Dao a) where { compare (Dao a) (Dao b) = compare a b     }
+instance             Functor     Dao    where { fmap fn (Dao a)         = Dao (fn a)      }
+instance Monoid a => Monoid     (Dao a) where
+  mempty              = Dao mempty
+  mappend (Dao a) (Dao b) = Dao (mappend a b)
+instance             Applicative Dao    where
+  pure                = Dao
+  (Dao fn) <*> (Dao a)    = Dao (fn a)
 
 randMaxDepth :: Int
 randMaxDepth = 6
 
 -- | Generate a random object of a polymorphic type with a specified maximum depth.
-ioRandOMax :: HasRandGen o => Int -> Int -> IO (O o)
-ioRandOMax max i = return (O (evalState randO (initRandOState max i)))
+ioRandOMax :: HasRandGen o => Int -> Int -> IO (Dao o)
+ioRandOMax max i = return (Dao (evalState randO (initRandOState max i)))
 
 -- | Generate a random object of a polymorphic type.
-ioRandO :: HasRandGen o => Int -> IO (O o)
+ioRandO :: HasRandGen o => Int -> IO (Dao o)
 ioRandO i = ioRandOMax randMaxDepth i
 
 -- | Generate a random 'Dao.Object.AST.AST_TopLevel' expression from a seed value.
-randTopExpr :: Int -> IO (O AST_TopLevel)
+randTopExpr :: Int -> IO (Dao AST_TopLevel)
 randTopExpr = ioRandO
 
 -- | Generate a random 'Dao.Object.AST.AST_Script' from a seed value.
-randScriptExpr :: Int -> IO (O AST_Script)
+randScriptExpr :: Int -> IO (Dao AST_Script)
 randScriptExpr = ioRandO
 
 -- | Generate a random 'Dao.Object.AST.AST_Object' from a seed value.
-randObjExpr :: Int -> IO (O AST_Object)
+randObjExpr :: Int -> IO (Dao AST_Object)
 randObjExpr = ioRandO
 
 -- | Generate a random 'Dao.Object.Object' value from a seed value.
-randObj :: Int -> IO (O Object)
+randObj :: Int -> IO (Dao Object)
 randObj = ioRandO
 
 -- | Generate a random 'Dao.Object.AST.AST_TopLevel' expression from a seed value with a specified
 -- maximum depth.
-randTopExprMax :: Int -> Int -> IO (O AST_TopLevel)
+randTopExprMax :: Int -> Int -> IO (Dao AST_TopLevel)
 randTopExprMax = ioRandOMax
 
 -- | Generate a random 'Dao.Object.AST.AST_Script' from a seed value with a specified maximum depth.
-randScriptExprMax :: Int -> Int -> IO (O AST_Script)
+randScriptExprMax :: Int -> Int -> IO (Dao AST_Script)
 randScriptExprMax = ioRandOMax
 
 -- | Generate a random 'Dao.Object.AST.AST_Object' from a seed value with a specified maximum depth.
-randObjExprMax :: Int -> Int -> IO (O AST_Object)
+randObjExprMax :: Int -> Int -> IO (Dao AST_Object)
 randObjExprMax = ioRandOMax
 
 -- | Generate a random 'Dao.Object.Object' value from a seed value with a specified maximum depth.
-randObjMax :: Int -> Int -> IO (O Object)
+randObjMax :: Int -> Int -> IO (Dao Object)
 randObjMax = ioRandOMax
 
 -- | Re-export 'Dao.PPring.prettyShow'
@@ -140,75 +140,75 @@ randObjMax = ioRandOMax
 -- prettyShow = Dao.PPrint.prettyShow
 
 -- | Pretty-print anything output by one of the functions in this module.
-pp :: PPrintable a => (O a) -> IO (O a)
-pp (O o) = putStrLn (prettyShow o) >> return (O o)
+pp :: PPrintable a => (Dao a) -> IO (Dao a)
+pp (Dao o) = putStrLn (prettyShow o) >> return (Dao o)
 
 -- | Convert an object to a string using the pretty printer.
-toString :: PPrintable a => (O a) -> IO (O String)
-toString (O o) = return (O (prettyShow o))
+toString :: PPrintable a => (Dao a) -> IO (Dao String)
+toString (Dao o) = return (Dao (prettyShow o))
 
 -- | Write the text from pretty-printing an object to a file. I have always disliked how the
 -- 'System.IO.IOMode' comes after the file path in the 'System.IO.openFile' function, so the
 -- 'System.IO.IOMode' and 'System.IO.FilePath' parameters are flipped.
-fileOutputTextWith :: (a -> String) -> IOMode -> FilePath -> O a -> IO (O a)
-fileOutputTextWith toString mode path (O obj) = openFile path mode >>= \h ->
-  hPutStr h (toString obj) >>= evaluate >> hFlush h >>= evaluate >> hClose h >> return (O obj)
+fileOutputTextWith :: (a -> String) -> IOMode -> FilePath -> Dao a -> IO (Dao a)
+fileOutputTextWith toString mode path (Dao obj) = openFile path mode >>= \h ->
+  hPutStr h (toString obj) >>= evaluate >> hFlush h >>= evaluate >> hClose h >> return (Dao obj)
 
 -- | Like 'fileOutputTextWith' but uses 'System.IO.WriteMode'.
-writeTextWith :: (a -> String) -> FilePath -> O a -> IO (O a)
+writeTextWith :: (a -> String) -> FilePath -> Dao a -> IO (Dao a)
 writeTextWith toString path obj = fileOutputTextWith toString WriteMode path obj
 
 -- | Like 'fileOutputTextWith' but uses 'System.IO.AppendMode'.
-appendTextWith :: (a -> String) -> FilePath -> O a -> IO (O a)
+appendTextWith :: (a -> String) -> FilePath -> Dao a -> IO (Dao a)
 appendTextWith toString path obj = fileOutputTextWith toString AppendMode path obj
 
 -- | Like 'writeTextWith' but uses 'Dao.PPrint.prettyShow'.
-writeText :: PPrintable a => FilePath -> O a -> IO (O a)
+writeText :: PPrintable a => FilePath -> Dao a -> IO (Dao a)
 writeText path obj = writeTextWith prettyShow path obj
 
 -- | Like 'appendTextWith' but uses 'Dao.PPrint.prettyShow'.
-appendText :: PPrintable a => FilePath -> O a -> IO (O a)
+appendText :: PPrintable a => FilePath -> Dao a -> IO (Dao a)
 appendText path obj = appendTextWith prettyShow path obj
 
 -- | Parser a polymorphic type from a string expression.
-parseWith :: Parser a -> O String -> IO (O a)
-parseWith parser (O str) = case runParser parser str of
-  (OK          a, _) -> seq a $! return (O a)
+parseWith :: Parser a -> Dao String -> IO (Dao a)
+parseWith parser (Dao str) = case runParser parser str of
+  (OK          a, _) -> seq a $! return (Dao a)
   (Backtrack    , s) -> error ("parser backtracks:\n\t"++show s)
   (PFail loc msg, s) -> error (show loc ++ ": "++uchars msg++"\n\t"++show s)
 
 -- | Parse a 'Dao.Object.AST.AST_TopLevel'.
-parseTopExpr :: O String -> IO (O AST_TopLevel)
+parseTopExpr :: Dao String -> IO (Dao AST_TopLevel)
 parseTopExpr = parseWith parseDirective
 
 -- | Parse a 'Dao.Object.AST.AST_Script'.
-parseScriptExpr :: O String -> IO (O AST_Script)
+parseScriptExpr :: Dao String -> IO (Dao AST_Script)
 parseScriptExpr = parseWith Dao.Object.Parser.parseScriptExpr
 
 -- | Parse a 'Dao.Object.AST.AST_Object'.
-parseObjExpr :: O String -> IO (O AST_Object)
+parseObjExpr :: Dao String -> IO (Dao AST_Object)
 parseObjExpr = parseWith (fmap fst parseObjectExpr)
 
 -- | Parse a 'Dao.Object.AST.AST_TopLevel' from an ordinary 'Prelude.String'.
-readTopExpr :: String -> IO (O AST_TopLevel)
-readTopExpr = parseTopExpr . O
+readTopExpr :: String -> IO (Dao AST_TopLevel)
+readTopExpr = parseTopExpr . Dao
 
 -- | Parse a 'Dao.Object.AST.AST_TopLeve' from an ordinary 'Prelude.String'.
-readScriptExpr :: String -> IO (O AST_Script)
-readScriptExpr = Dao.Prelude.parseScriptExpr . O
+readScriptExpr :: String -> IO (Dao AST_Script)
+readScriptExpr = Dao.Prelude.parseScriptExpr . Dao
 
 -- | Parse a 'Dao.Object.AST.AST_TopLeve' from an ordinary 'Prelude.String'.
-readObjExpr :: String -> IO (O AST_Object)
-readObjExpr = parseObjExpr . O
+readObjExpr :: String -> IO (Dao AST_Object)
+readObjExpr = parseObjExpr . Dao
 
 -- | Convert a structured item to its structured form.
-toStruct :: Structured o => (O o) -> IO (O (T.Tree Name Object))
-toStruct (O o) = return (O (dataToStruct o))
+toStruct :: Structured o => (Dao o) -> IO (Dao (T.Tree Name Object))
+toStruct (Dao o) = return (Dao (dataToStruct o))
 
 -- | Construct a random object of a polymorphic type from its 'Dao.Struct.Structured' form.
-fromStruct :: Structured o => (O (T.Tree Name Object)) -> IO (O o)
-fromStruct (O t) = case structToData t of
-  OK              o   -> seq o $! return (O o)
+fromStruct :: Structured o => (Dao (T.Tree Name Object)) -> IO (Dao o)
+fromStruct (Dao t) = case structToData t of
+  OK              o   -> seq o $! return (Dao o)
   Backtrack           -> error "constructor backtracked"
   PFail (idx,obj) msg -> error $ concat $
     [ "constructor failed: ", uchars msg
@@ -217,87 +217,87 @@ fromStruct (O t) = case structToData t of
     ]
 
 -- | Construct a random 'Dao.Object.AST.AST_TopLevel' expression from its 'Dao.Struct.Structured' form.
-structTopExpr :: O (T.Tree Name Object) -> IO (O AST_TopLevel)
+structTopExpr :: Dao (T.Tree Name Object) -> IO (Dao AST_TopLevel)
 structTopExpr = fromStruct
 
 -- | Construct a random 'Dao.Object.AST.AST_Script' expression from its 'Dao.Struct.Structured' form.
-structScriptExpr :: O (T.Tree Name Object) -> IO (O AST_Script)
+structScriptExpr :: Dao (T.Tree Name Object) -> IO (Dao AST_Script)
 structScriptExpr = fromStruct 
 
 -- | Construct a random 'Dao.Object.AST.AST_Object' expression from its 'Dao.Struct.Structured' form.
-structObjExpr :: O (T.Tree Name Object) -> IO (O AST_Object)
+structObjExpr :: Dao (T.Tree Name Object) -> IO (Dao AST_Object)
 structObjExpr = fromStruct 
 
 -- | Convert a polymorphic type to a 'Data.ByteString.Lazy.ByteString'.
-toBinary :: Binary o => O o -> IO (O B.ByteString)
-toBinary (O o) = return (O (encode o))
+toBinary :: Binary o => Dao o -> IO (Dao B.ByteString)
+toBinary (Dao o) = return (Dao (encode o))
 
 -- | Convert a polymorphic type to a 'Data.ByteString.Lazy.ByteString'.
-fromBinary :: Binary o => O B.ByteString -> IO (O o)
-fromBinary (O o) = return (O (decode o))
+fromBinary :: Binary o => Dao B.ByteString -> IO (Dao o)
+fromBinary (Dao o) = return (Dao (decode o))
 
 -- | Parse a 'Dao.Object.TopLevelExpr' from it's binary representation.
-unpackTopExpr :: O B.ByteString -> IO (O TopLevelExpr)
+unpackTopExpr :: Dao B.ByteString -> IO (Dao TopLevelExpr)
 unpackTopExpr = fromBinary
 
 -- | Parse a 'Dao.Object.ScriptExpr' from it's binary representation.
-unpackScriptExpr :: O B.ByteString -> IO (O ScriptExpr)
+unpackScriptExpr :: Dao B.ByteString -> IO (Dao ScriptExpr)
 unpackScriptExpr = fromBinary
 
 -- | Parse a 'Dao.Object.ScriptExpr' from it's binary representation.
-unpackObjExpr :: O B.ByteString -> IO (O ObjectExpr)
+unpackObjExpr :: Dao B.ByteString -> IO (Dao ObjectExpr)
 unpackObjExpr = fromBinary
 
 -- | Parse a 'Dao.Object.ScriptExpr' from it's binary representation.
-unpackObj :: O B.ByteString -> IO (O Object)
+unpackObj :: Dao B.ByteString -> IO (Dao Object)
 unpackObj = fromBinary
 
 -- | Reduce a Dao language expression from it's "Dao.Object.AST" representation to it's "Dao.Object"
 -- representation using its instantiation of methods 'Dao.Object.AST.Intermediate'. See also:
 -- 'expand', the inverse of this function.
-reduce :: Intermediate obj ast => O ast -> IO (O obj)
-reduce (O ast) = case toInterm ast of
-  [obj] -> return (O obj)
+reduce :: Intermediate obj ast => Dao ast -> IO (Dao obj)
+reduce (Dao ast) = case toInterm ast of
+  [obj] -> return (Dao obj)
   []    -> error "could not reduce"
   _     -> error "ambiguous reduction"
 
 -- | Apply 'reduce' to an 'Dao.Object.AST.AST_TopLevel' expression, producing an
 -- 'Dao.Object.TopLevelExpr'.
-reduceTopExpr :: O AST_TopLevel -> IO (O TopLevelExpr)
+reduceTopExpr :: Dao AST_TopLevel -> IO (Dao TopLevelExpr)
 reduceTopExpr = reduce
 
 -- | Apply 'reduce' to an 'Dao.Object.AST.AST_Script' expression, producing an
 -- 'Dao.Object.ScriptExpr'.
-reduceScriptExpr :: O AST_TopLevel -> IO (O TopLevelExpr)
+reduceScriptExpr :: Dao AST_TopLevel -> IO (Dao TopLevelExpr)
 reduceScriptExpr = reduce
 
 -- | Apply 'reduce' to an 'Dao.Object.AST.AST_Object' expression, producing an
 -- 'Dao.Object.ObjectExpr'.
-reduceObjExpr :: O AST_TopLevel -> IO (O TopLevelExpr)
+reduceObjExpr :: Dao AST_TopLevel -> IO (Dao TopLevelExpr)
 reduceObjExpr = reduce
 
 -- | Expand a Dao language expression into it's "Dao.Object.AST" representation from it's
 -- "Dao.Object" representation using its instantiation of methods 'Dao.Object.AST.Intermediate'. See
 -- also: 'reduce', the inverse of this function.
-expand :: Intermediate obj ast => O obj -> IO (O ast)
-expand (O obj) = case fromInterm obj of
-  [ast] -> return (O ast)
+expand :: Intermediate obj ast => Dao obj -> IO (Dao ast)
+expand (Dao obj) = case fromInterm obj of
+  [ast] -> return (Dao ast)
   []    -> error "could not expand"
   _     -> error "ambiguous expansion"
 
 -- | Apply 'expand' to an 'Dao.Object.AST.AST_TopLevel' expression, producing an
 -- 'Dao.Object.TopLevelExpr'.
-expandTopExpr :: O TopLevelExpr -> IO (O AST_TopLevel)
+expandTopExpr :: Dao TopLevelExpr -> IO (Dao AST_TopLevel)
 expandTopExpr = expand
 
 -- | Apply 'expand' to an 'Dao.Object.AST.AST_Script' expression, producing an
 -- 'Dao.Object.ScriptExpr'.
-expandScriptExpr :: O ScriptExpr -> IO (O AST_Script)
+expandScriptExpr :: Dao ScriptExpr -> IO (Dao AST_Script)
 expandScriptExpr = expand
 
 -- | Apply 'expand' to an 'Dao.Object.AST.AST_Object' expression, producing an
 -- 'Dao.Object.ObjectExpr'.
-expandObjExpr :: O ObjectExpr -> IO (O AST_Object)
+expandObjExpr :: Dao ObjectExpr -> IO (Dao AST_Object)
 expandObjExpr = expand
 
 -- | Pure function, convert a 'Data.ByteString.Lazy.ByteString' to a string that is easy to look
@@ -308,6 +308,6 @@ showBinary b = intercalate "\n" $ breakInto (32*3) $ (" "++) $ map toUpper $
   intercalate " " $ map (\b -> (if b<0x10 then ('0':) else id) (flip showHex "" b)) $ B.unpack b
 
 -- | Apply 'showBinary' to a 'Data.ByteString.Lazy.ByteString'.
-hexdump :: O B.ByteString -> IO (O B.ByteString)
-hexdump (O bytes) = putStrLn (showBinary bytes) >> return (O bytes)
+hexdump :: Dao B.ByteString -> IO (Dao B.ByteString)
+hexdump (Dao bytes) = putStrLn (showBinary bytes) >> return (Dao bytes)
 

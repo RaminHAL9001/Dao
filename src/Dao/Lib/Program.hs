dc199c646838b0e4a46d5cb69c1b08cebf595740
Ramin Honary <ramin.honary@gmail.com>
2013-08-27 03:21:42 +0900

    I have my work cut out for me -- or, I have cut my work out for me. I have committed to the repository a set fundamental data types that will make up the Dao standard library. This is only what has come to mind so far, I may extend or reduce these items.

---
+++ src/Dao/Lib/Program.hs
@@ -0,0 +1,23 @@
+-- "src/Dao/Lib/Program.hs"  built-in object for Dao programs that can
+-- be edited and run in the same runtime that is editing them, allowing
+-- a larger program to "learn" new rules.
+-- 
+-- Copyright (C) 2008-2013  Ramin Honary.
+-- This file is part of the Dao System.
+--
+-- The Dao System is free software: you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+-- 
+-- The Dao System is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with this program (see the file called "LICENSE"). If not, see
+-- <http://www.gnu.org/licenses/agpl.html>.
+
+
+
